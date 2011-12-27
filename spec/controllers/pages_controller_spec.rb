57af4aeb9c6a88113d02de439da0d27125e38fa2
Xun Xu <seanking1983@gmail.com>
2011-12-27 07:22:50 -1000

    3. + tests for pages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,32 +2,51 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
+
+# link test
+
     it "should be successful" do
-      get 'home'
+      get :home
       response.should be_success
     end
-  end
+  
 
-  describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
+      get :contact
       response.should be_success
     end
-  end
+  
 
-  describe "GET 'about'" do
     it "should be successful" do
-      get 'about'
+      get :about
       response.should be_success
     end
-  end
+  
 
-  describe "GET 'help'" do
     it "should be successful" do
-      get 'help'
+      get :help
       response.should be_success
     end
-  end
 
+# title test
+    
+    it "should have the right title for home" do
+      get :home
+      response.should have_selector('title', :content => "Home")
+    end
+
+    it "should have the right title for home" do
+      get :contact
+      response.should have_selector('title', :content => "Contact Us")
+    end
+    
+    it "should have the right title for home" do
+      get :about
+      response.should have_selector('title', :content => "About Us")
+    end
+    
+    it "should have the right title for home" do
+      get :help
+      response.should have_selector('title', :content => "Help")
+    end
 end
