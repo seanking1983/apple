4840ea0fda3cd388d3943deaedbf954505b53686
Xun Xu <seanking1983@gmail.com>
2011-12-27 22:06:43 -1000

    19. + factories.rb to generate a test user to test show template, tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
-  describe "GET 'new'" do
+  describe "get new" do
     it "should be successful" do
       get :new
       response.should be_success
@@ -13,6 +13,24 @@ describe UsersController do
       get :new
       response.should have_selector('title', :content => "Sign Up")
     end
+  end
+  
+  
+  describe "get show" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  
   end
 
 end
