8daed2545b59d953a284a7e2f057f5bd699bd6d9
Xun Xu <seanking1983@gmail.com>
2011-12-28 12:52:09 -1000

    29. + integration test for users controller focusing on signup failure and success

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Users" do
+  
+  
+  # signup failure
+    it "should not make a user" do
+      lambda do
+        visit signup_path
+        fill_in "Name",             :with => ""
+        fill_in "Email",            :with => ""
+        fill_in "Password",         :with => ""
+        fill_in "Retype Password",  :with => ""
+        click_button
+        response.should render_template('users/new')
+        response.should have_selector('div#error_explanation')
+      end.should_not change(User, :count)
+    end
+  
+  
+  # signup success
+  
+    it "should make a new user" do
+      lambda do
+        visit signup_path
+        fill_in "Name",             :with => "Xun Xu"
+        fill_in "Email",            :with => "xunx@hawaii.edu"
+        fill_in "Password",         :with => "hawaii123456"
+        fill_in "Retype Password",  :with => "hawaii123456"
+        click_button
+        response.should have_selector('div.flash.success', :content => "Welcome")
+        response.should render_template('users/show')
+      end.should change(User, :count).by(1)
+    end
+end
