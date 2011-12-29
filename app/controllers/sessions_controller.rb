97dddfe325a65e39d6eb03fdb162164e5acc3c28
Xun Xu <seanking1983@gmail.com>
2011-12-28 21:03:04 -1000

    32. + test for signin failure, + code for signin failure in sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,16 @@ class SessionsController < ApplicationController
   def new
     @title = "Sign In"
   end
+  
+  def create
+     user = User.authenticate(params[:session][:email],
+                              params[:session][:password])
+      if user.nil?
+        flash.now[:error] = "Invalid email/password combination."
+        render 'new'
+      else
+
+      end
+  end
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe SessionsController do
   render_views
+  
+    
+    # new/signin form
 
     it "should be successful" do
       get :new
@@ -12,6 +15,26 @@ describe SessionsController do
       get :new
       response.should have_selector('title', :content => "Sign In")
     end
+    
+    
+    # create/signin
+    describe "signin failure" do
+      before(:each) do
+        @attr = {:email => "", :password => ""}
+      end
+
+      it "should render the new page if failed" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    end
+    
+    
   
 
 end
